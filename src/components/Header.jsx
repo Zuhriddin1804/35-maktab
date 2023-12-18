5b40e2d55fca375aff4ba3b05f6d75db57f6954c
Zuhriddin1804 <sotvoldiyevzuhriddin52@gmail.com>
2023-12-18 18:18:03 +0500

    Added NavlInks to Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 const Header = () => {
     return (
@@ -8,6 +8,22 @@ const Header = () => {
                 <div>
                     <Link className='text-white text-3xl font-medium leading-normal' to='/'>35-MAKTAB</Link>
                 </div>
+                <nav>
+                    <ul className='flex items-center text-white text-lg font-medium leading-normal space-x-5 hover:text-red-600 duration-150'>
+                        <li>
+                            <NavLink to='/'>Bosh sahifa</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/'>Maktab haqida</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/'>Tadbirlar</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/'>Aloqa</NavLink>
+                        </li>
+                    </ul>
+                </nav>
             </div>
         </header>
     );
