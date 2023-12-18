18b21092e7be91f5fe71d80a0e8d0915f79fa99d
Zuhriddin1804 <sotvoldiyevzuhriddin52@gmail.com>
2023-12-18 10:04:15 +0500

    Add header logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <header className='w-full bg-gray-800 py-5'>
             <div className='container'>
-                
+                <div>
+                    <Link className='text-white text-3xl font-medium leading-normal' to='/'>35-MAKTAB</Link>
+                </div>
             </div>
         </header>
     );
