31d038e5136d3cb6754b3c8fec6e4f5b240efd52
Zuhriddin1804 <sotvoldiyevzuhriddin52@gmail.com>
2023-12-18 18:23:28 +0500

    The header has been styled

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import { Link, NavLink } from 'react-router-dom';
 const Header = () => {
     return (
         <header className='w-full bg-gray-800 py-5'>
-            <div className='container'>
+            <div className='flex justify-between items-center container'>
                 <div>
                     <Link className='text-white text-3xl font-medium leading-normal' to='/'>35-MAKTAB</Link>
                 </div>
