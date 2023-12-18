5ed4845eaa2ba2c08be6af3dad8f64b3b3ad57a7
Zuhriddin1804 <sotvoldiyevzuhriddin52@gmail.com>
2023-12-18 09:20:10 +0500

    Add site header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <>
-        </>
+        <header className='w-full bg-gray-800 py-5'>
+            <div className='container'>
+                
+            </div>
+        </header>
     );
 };
 
