97d6eb0bc07fbce8ff80aa833fbe5f39f28db745
Zuhriddin1804 <sotvoldiyevzuhriddin52@gmail.com>
2023-12-18 18:36:36 +0500

    About, Event, Contact pages have been routed

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,18 @@ import {
 } from "react-router-dom";
 import RootLayout from './layouts/RootLayout';
 import Home from './pages/Home';
+import About from './pages/About';
+import Event from './pages/Event';
+import Contact from './pages/Contact';
 
 const App = () => {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route element={<RootLayout />}>
         <Route index element={<Home />} />
+        <Route path='/About' element={<About />} />
+        <Route path='/Event' element={<Event />} />
+        <Route path='/Contact' element={<Contact />} />
       </Route>
     )
   );
